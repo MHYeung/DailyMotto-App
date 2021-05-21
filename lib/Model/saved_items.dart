b3ae180bd0f4a36f858debf07e75959934fc4bfd
MHYeung <johnyeung1998@gmail.com>
2021-05-21 23:56:34 +0800

    ok

--- lib/Model/saved_items.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class SavedItems{
-  final List<String> savedquotes;
-  final List<String> quotesid;
-
-
-  SavedItems({this.savedquotes, this.quotesid});
-}
